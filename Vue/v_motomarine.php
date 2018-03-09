7a4bcee540796d84cd9419860e10da7c337ea0e1
Natoh <35684695+Natoh@users.noreply.github.com>
2018-03-09 16:44:04 +0100

    footer and motoM

--- Vue/v_motomarine.php
+++ Vue/v_motomarine.php
@@ -1,7 +1,218 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Quentin
- * Date: 05/03/2018
- * Time: 10:39
- */
+<div>
+    <H1 class="header center light-blue-text text-darken-4">MotoMarines</H1>
+    <div class="container">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie, tempor lacus mattis, vehicula dolor. Nulla lacinia,
+            ipsum in dapibus eleifend, elit ex tincidunt tortor, sit amet tristique mauris massa non lacus.  Fusce placerat vitae turpis ut aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eu efficitur felis. Integer dignissim vulputate convallis.</p>
+    </div>
+    <!--    <div class="divider"></div>-->
+
+    <div class="container">
+
+        <div class="row">
+            <form class="col m12">
+                <div class="input-field col m4">
+                    <select>
+                        <option value="" disabled selected>Trier par Marque</option>
+                        <option value="1">See Doo</option>
+                        <option value="2">Yamaaha</option>
+                        <option value="3">Pacific craft</option>
+                    </select>
+                    <label>Marque</label>
+                </div>
+                <div class="input-field col m4">
+                    <select>
+                        <option value="" disabled selected>Trier par Prix</option>
+                        <option value="1">Croissant</option>
+                        <option value="2">Decroissant</option>
+                        <option value="3">Promotions</option>
+                    </select>
+                    <label>Prix</label>
+                </div>
+                <div class="input-field col m4">
+                    <select>
+                        <option value="" disabled selected>Trier par Nom</option>
+                        <option value="1">Croissant</option>
+                        <option value="2">Decroissant</option>
+                    </select>
+                    <label>Nom</label>
+                </div>
+                <!--                <select class="browser-default" disabled>-->
+                <!--                    <option value="" disabled selected>Choose your option</option>-->
+                <!--                    <option value="1">Option 1</option>-->
+                <!--                    <option value="2">Option 2</option>-->
+                <!--                    <option value="3">Option 3</option>-->
+                <!--                </select>-->
+            </form>
+        </div>
+
+
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/PWC family_2J8A7808_MY18.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/PWC family_2J8A7808_MY18.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/pic-16-610x428.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/PWC family_2J8A7808_MY18.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/PWC family_2J8A7808_MY18.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Util/img/PWC family_2J8A7808_MY18.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col m5 ">
+                <div class="card-panel">
+                    <H3> Nom MotoMarine</H3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam volutpat tellus vitae rutrum. Pellentesque ut quam molestie,
+                        tempor lacus mattis, vehicula dolor. Nulla laciniaipsum in dapibus eleifend, elit ex tincidunt tortor,</p>
+                    <p>ref : 445223</p>
+                    <p>puissance : 40000cv</p>
+                    <p>poids : 2kg</p>
+                    <p>taille : 1/5/6</p>
+                    <H5 class="right-align">PRIX</H5>
+                    <a href="" class="waves-effect waves-light btn">Fiche produit</a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+
+
+        <ul class="pagination">
+            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+    </div><!-- container -->
+</div>
+<i class="material-icons" onclick="topFunction()" id="myBtn" title="Go to top">navigation</i>
+<script>
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+</script>
