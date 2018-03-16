57050825448e8d7a972696db96a9d3945eb6f486
Natoh <35684695+Natoh@users.noreply.github.com>
2018-03-16 16:02:16 +0100

    correction bug jquery

--- Vue/Structure/v_footer.php
+++ Vue/Structure/v_footer.php
@@ -41,11 +41,5 @@
         </div>
     </div>
 </footer>
-
-<!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="Util/JS_AJAX/materialize.min.js"></script>
-
-
 </body>
 </html>
