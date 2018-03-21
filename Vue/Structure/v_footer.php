e38f2fd673d70ccd738d99ba8115e6d61b2b6d82
guyonnet nautic <quentin.guyonnet@greta-sud-aquitaine.academy>
2018-03-21 19:16:09 +0100

    fin accueil

--- Vue/Structure/v_footer.php
+++ Vue/Structure/v_footer.php
@@ -3,7 +3,6 @@
         <div class="row">
             <div class="col m3">
                 <h5 class="white-text">Guyonnet Nautic</h5>
-                <p class="grey-text text-lighten-4">Ceci est le footer.</p>
             </div>
             <div class="col m3">
                 <h5 class="white-text">Horaires</h5>
@@ -17,7 +16,7 @@
                 <h5 class="white-text">Categories</h5>
                 <ul>
                     <li><i class="material-icons tiny">send</i><a class="grey-text text-lighten-3" href="index.php?c=bateau&a=afficher">Bateau</a></li>
-                    <li><i class="material-icons tiny">send</i><a class="grey-text text-lighten-3" href="index.php?c=motomarine&a=afficher">Motomarine</a></li>
+                    <li><i class="material-icons tiny">send</i><a class="grey-text text-lighten-3" href="index.php?c=motomarine&a=afficher">Jet</a></li>
                     <li><i class="material-icons tiny">send</i><a class="grey-text text-lighten-3" href="index.php?c=moteur&a=afficher">Moteur</a></li>
                     <li><i class="material-icons tiny">send</i><a class="grey-text text-lighten-3" href="index.php?c=permis&a=afficher">Permis</a></li>
                 </ul>
@@ -25,16 +24,16 @@
             <div class="col m3">
                 <h5 class="white-text">Contact</h5>
                 <ul>
-                    <li><a class="grey-text text-lighten-3" >Telephone</a></li>
-                    <li><a class="grey-text text-lighten-3" >Adresse</a></li>
-                    <li><a class="grey-text text-lighten-3" >Mail</a></li>
+                    <li><a class="grey-text text-lighten-3" >Telephone : 05.59.30.71.98</a></li>
+                    <li><a class="grey-text text-lighten-3" >Adresse : Avenue du Corps Franc Pommies, 64320 BIZANOS</a></li>
+                    <li><a class="grey-text text-lighten-3" >Mail : contact@guyonnetnautic.com</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
-            © 2014 Copyright Text
+            © 2018 Copyright Text
         </div>
     </div>
 </footer>
